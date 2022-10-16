d499055300af5961e324b9e1ed493f2a42f1fc64
User <User@DESKTOP-49HMLNI>
2022-10-16 22:00:24 +0800

    加入專案檔案。

---
+++ Exec_For05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For05
+{/*用迴圈呈現指定下方的結果
+	++++1
+	+++22
+	++333
+	+4444
+	5555 */
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int length = 5;
+			for (int i = 1; i <= length; i++)
+			{
+				//加號的數量
+				int countOfPlus = length - i;
+
+				//數字的數量
+				int countOfNumber = i;
+				string stringOfNumber = $"{i}";
+				//字串轉字元
+				char charOfNumber = stringOfNumber[0];
+
+				string row = new string('+', countOfPlus)+ new string(charOfNumber, countOfNumber);
+				Console.WriteLine(row);
+			}
+		}
+	}
+}
